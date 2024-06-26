ae5c386ade2ac91eb24e0b2d6162a49f3845ef7f
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-06-26 17:45:19 +0100

    creación básica de componenentes contact, blog y trajectory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PortafolioComponent } from './portafolio/portafolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { TrajectoryComponent } from './trajectory/trajectory.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'portafolio', component: PortafolioComponent}
+    {path: 'portafolio', component: PortafolioComponent},
+    {path: 'blog', component: BlogComponent},
+    {path: 'trajectory', component: TrajectoryComponent},
+    {path: 'contact', component: ContactComponent}
 ];
---
+++ src/app/trajectory/trajectory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trajectory',
+  standalone: true,
+  imports: [],
+  templateUrl: './trajectory.component.html',
+  styleUrl: './trajectory.component.css'
+})
+export class TrajectoryComponent {
+
+}

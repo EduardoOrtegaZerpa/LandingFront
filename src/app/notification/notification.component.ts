724d190dea18f18c9fc4e3b751b2d67729204ec0
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-07-15 23:44:11 +0100

    notifications logic and css fixed

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NotificationService } from './notification.service';
 import { CommonModule } from '@angular/common';
+import { LoadingService } from '../loading/loading.service';
 
 @Component({
   selector: 'app-notification',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './notification.component.html',
-  styleUrl: './notification.component.css'
+  styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit, OnDestroy {
 
   isShown: boolean = false;
   message: string = '';
   animateOut: boolean = false;
   isError: boolean = false;
+  isLoading: boolean = false;
+  private timeoutRef: any;
 
-  constructor(private notificationService: NotificationService) { }
+  constructor(
+    private notificationService: NotificationService,
+    private loadingService: LoadingService
+  ) { }
 
   ngOnInit(): void {
-    this.notificationService.isNotificationShown().subscribe(isShown => {
-      if (isShown) {
-        this.isShown = true;
-        this.animateOut = false;
-        setTimeout(() => {
-          this.animateOut = true;
-          setTimeout(() => {
-            this.isShown = false;
-            this.animateOut = false;
-          }, 500);
-        }, 3000);
-      }
+    this.loadingService.isLoading$().subscribe(isLoading => {
+      this.isLoading = isLoading;
+      this.checkNotification();
     });
+
+    this.checkNotification();
 
     this.notificationService.getNotificationMessage().subscribe(message => {
       this.message = message;
@@ -42,10 +41,37 @@ export class NotificationComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.clearTimeout();
+  }
+
+  checkNotification() {
+    this.notificationService.isNotificationShown().subscribe(isShown => {
+      if (isShown && !this.isLoading) {
+        this.isShown = true;
+        this.animateOut = false;
+        this.clearTimeout();
+        this.timeoutRef = setTimeout(() => {
+          this.animateOut = true;
+          this.timeoutRef = setTimeout(() => {
+            this.close();
+          }, 500);
+        }, 3000);
+      }
+    });
+  }
+
   close() {
+    this.clearTimeout();
     this.isShown = false;
     this.animateOut = false;
     this.notificationService.hide();
   }
 
+  private clearTimeout() {
+    if (this.timeoutRef) {
+      clearTimeout(this.timeoutRef);
+      this.timeoutRef = null;
+    }
+  }
 }

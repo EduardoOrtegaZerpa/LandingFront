8197988bff2620714794a603a433524b493f28c2
EduardoOrtegaZerpa <eduortza@gmail.com>
2024-07-31 22:05:03 +0100

    changed api config

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,6 +1,6 @@
 export const config = {
     useLocalhost: false,
     API_URL: function() {
-      return this.useLocalhost ? 'http://localhost:8080' : '/api';
+      return this.useLocalhost ? 'http://localhost:8080' : 'https://eduortza.com:8443';
     }
   };
